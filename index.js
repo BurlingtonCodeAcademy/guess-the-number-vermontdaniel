92e7852a776be6928393f0838fa8128a44a2e523
Daniel <vermont.daniel.johnson@gmail.com>
2019-10-07 12:59:26 -0400

    Now can loop

--- index.js
+++ index.js
@@ -9,28 +9,34 @@ function ask(questionText) {
 function getRandomNumber(number) {
   let range = [...Array(100).keys()].map(i => ++i);
   return range[Math.floor(Math.random() * range.length)];
-    }
+}
 
 
 start();
 
 async function start() {
+
   console.log("Let's play a game where you (human) make up a number and I (computer) try to guess it.")
   let secretNumber = await ask("What is your secret number?\nI won't peek, I promise...\n");
   console.log('You entered: ' + secretNumber);
-  let guessedNumber = await ask(`Is the number ${getRandomNumber()}? `)
-    if (guessedNumber == 'no') {
-      console.log('Is it higher or lower?');
-    } else {
-      console.log('Yes that is the number!')
-    }
-    if (guessedNumber == 'higher')
+ 
+  let guessedNumber = await ask(`Is the number ${getRandomNumber()}? `);
+    while (guessedNumber !== 'yes') {
+
+      let higherOrLower = await ask('Is it higher or lower? ');
+      if (higherOrLower == 'higher') {
+        await ask(`Is the number ${getRandomNumber()}? `);
+      } else if (higherOrLower == 'lower') {
+        await ask(`Is the number ${getRandomNumber()}? `);
+       } else {
+        console.log('Yes that is the answer!');
+        return;
+       }
+        
+      }}
+      
     
-    } 
+  
+
+    
   //process.exit();
-
-
-//async function guessNumber() {
-//  let guessAnswer = await ask(`Is your number: ${getRandomNumber()}? `);
-//  return guessAnswer; 
-//}
