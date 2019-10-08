d6b1eee8fc2d6740e6ae608f0defcab796b9e9a2
Daniel <vermont.daniel.johnson@gmail.com>
2019-10-08 17:38:59 -0400

    save version before testing something drastic

--- index.js
+++ index.js
@@ -15,24 +15,41 @@
   start();
   
   async function start() {
-    console.log("Let's play a game where you (human) make up a number and I (computer) try to guess it.")
-    const secretNumber = await ask("What is your secret number?\nI won't peek, I promise...\n");
-    console.log('You entered: ' + secretNumber);
-    const guessedNumber = await ask(`Is your number ${randomInteger(1, 100)}? `);
-    
 
-    while (guessedNumber !== 'yes') {
-        let highOrLow = await ask('Is it higher or lower? ');
+    console.log("Let's play a game where you (human) make up a number and I (computer) try to guess it.")
+    let secretNumber = await ask("What is your secret number?\nI won't peek, I promise...\n");
+    console.log('You entered: ' + secretNumber);
+    let guessedNumber = await ask(`Is your number ${randomInteger(1, 100)}? `); //gets first random number
+    let highOrLow = await ask('Is it higher or lower? ');
+
+    while (highOrLow === 'higher' || 'lower') {
+      
+    }
+
+
+    while (guessedNumber !== 'yes') { 
+        
+        let min = 1; 
+        let max = 100;
+        let currentGuess = guessedNumber  //unsure if this needs to be here
 
         if (highOrLow === 'higher') {
+        min = currentGuess; //turn min into guessedNumber
+        max = secretNumber;
         console.log('You need to guess higher.');
-        await ask(`Is your number ${randomInteger(guessedNumber, secretNumber)}? `);
+        currentGuess = await ask(`Is your number ${randomInteger(min, max)}? `);
+        guessedNumber = currentGuess; // try to make currentGuess back into GUessed number to continue loop
+        
       } else if (highOrLow === 'lower') {
+        min = secretNumber;
+        max = currentGuess; //turn max into guessedNumber
         console.log('You need to guess lower.');
-        await ask(`Is your number ${randomInteger(secretNumber, guessedNumber)}? `);
+        currentGuess = await ask(`Is your number ${randomInteger(min, max)}? `); 
+        guessedNumber = currentGuess; //try to make currentGuess back into guessedNumber
+
       } else
       return;
-      }
-    }
+      }}
+    
     //process.exit();
   
